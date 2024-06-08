761fd4a0c67c8210266f2c5b83fe3012e928765a
MaximGordiyenko <maxim.gordiyenko@gmail.com>
2024-06-08 17:08:56 +0300

    fix: v2 of import fonts

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { NetworkingPage } from './pages/networking-page/NetworkingPage.jsx';
 import { TechPage } from './pages/tech-page/TechPage.jsx';
 import { AboutPage } from './pages/about-page/AboutPage.jsx';
 import './App.css';
-import './assets/fonts.css';
+import '../public/fonts.css';
 
 export const App = () => {
   
